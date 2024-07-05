cca87ae0694103cd02da00ff5d9baf85ac66ea1d
Mohammad Roohullah <72871047+MR-514@users.noreply.github.com>
2024-07-05 12:36:42 +0530

    Delete agriaid directory

--- agriaid/src/utils/voucherifyConfig.ts
+++
@@ -1,9 +0,0 @@
-import { VoucherifyServerSide } from '@voucherify/sdk'
-
-const  voucherifyClient = VoucherifyServerSide({
-    applicationId: '7cf1fb25-a5bf-44e7-9113-cb05e738526a',
-    secretKey: '52d01ef5-026d-4308-8ccb-2e143209d619',
-    apiUrl: 'https://api.voucherify.io', 
-})
-
-export default voucherifyClient
